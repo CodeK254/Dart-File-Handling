68f42fa8d28416bdd76ae8608055373e5bb7652d
CodeK254 <codekarma1003@gmail.com>
2023-11-05 16:58:50 +0300

    Initial Commit

--- File_Handling_02/pick_file.dart
+++ File_Handling_02/pick_file.dart
@@ -4,7 +4,7 @@ import 'dart:typed_data';
 
 void main() async {
   File file = File("new_file.txt");
-  file.writeAsStringSync("This is a simple text. Nothing much.");
+  file.writeAsStringSync("This is a simple text. Nothing much!!!");
 
   File fileTwo = File("new_file_two.txt");
   String text = base64Encode(file.readAsBytesSync());
